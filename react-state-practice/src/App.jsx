36305d2c50988ebce48c227e2625693b291a1a0b
AniketChile <84616134+AniketChile@users.noreply.github.com>
2024-12-21 09:19:42 +0530

    useContext implemented

--- react-state-practice/src/App.jsx
+++ react-state-practice/src/App.jsx
@@ -5,16 +5,25 @@ import UseEffect from './components/UseEffect'
 import UseRef from './components/UseRef'
 import UseMemoUsage from './components/UseMemoUsage'
 import UseCallbackUsage from './components/UseCallbackUsage'
+import Profile from './components/Profile'
+import Footer from './components/Footer'
+import UseContextUsage from './components/UseContextUsage'
+import ContextProvider from './context/UserContext'
 
 function App() {
   
   
   return (
+    <ContextProvider>
+      <UseContextUsage/>
+    </ContextProvider>
     // <UseStateUsage/>
     // <UseEffect/>
     // <UseRef/>
     // <UseMemoUsage/>
-    <UseCallbackUsage/>
+    // <UseCallbackUsage/>
+    
+    
   )
 }
 
